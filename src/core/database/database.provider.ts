7edb2324fc9ee59862ad9f935ec222452dcb3e2d
PiotrKukuc12 <PiotrKukuc12@gmail.com>
2022-01-14 00:50:02 +0100

    Create database module with connection via elephant SQL

---
+++ src/core/database/database.provider.ts
@@ -0,0 +1,18 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProvider = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async () =>
+      await createConnection({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [],
+        synchronize: true,
+      }),
+  },
+];
